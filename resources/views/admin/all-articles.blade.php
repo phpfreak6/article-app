fd5735bcb5ac83cb4eb314389d70b1d9f191f22a
phpfreak6 <php.freak6@gmail.com>
2022-10-28 18:19:39 +0530

    Update all-articles.blade.php

--- resources/views/admin/all-articles.blade.php
+++ resources/views/admin/all-articles.blade.php
@@ -14,6 +14,16 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
+    @if(session('success'))
+	<div class="alert alert-block alert-success">
+    <button type="button" class="close" data-dismiss="alert">
+        <i class="ace-icon fa fa-times"></i>
+    </button>
+	<i class="ace-icon fa fa-check green"></i>
+    {{session('success')}}
+
+</div>
+@endif
     @if(session('error'))
 	<div class="alert alert-block alert-danger">
     <button type="button" class="close" data-dismiss="alert">
